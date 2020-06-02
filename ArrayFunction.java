b311af905c7259afd3fb46e8c26b79236d1aee34
ritikakulkarni <65144317+ritikakulkarni@users.noreply.github.com>
2020-06-02 13:27:33 +0530

    Add files via upload

---
+++ ArrayFunction.java
@@ -0,0 +1,53 @@
+
+public class ArrayFuncion { 
+
+	
+	public static void sumSubsets( 
+		int set[], int n, int target) 
+	{ 
+		int x[] = new int[set.length]; 
+		int j = set.length - 1; 
+
+		// Convert the array into binary array 
+		while (n > 0) { 
+			x[j] = n % 2; 
+			n = n / 2; 
+			j--; 
+		} 
+
+		int sum = 0; 
+
+		// Calculate the sum of this subset 
+		for (int i = 0; i < set.length; i++) 
+			if (x[i] == 1) 
+				sum = sum + set[i]; 
+
+		if (sum == target) { 
+			System.out.print("{"); 
+			for (int i = 0; i < set.length; i++) 
+				if (x[i] == 1) 
+					System.out.print(set[i] + ", "); 
+			System.out.print("}, "); 
+		} 
+	} 
+
+	public static void findSubsets(int[] arr, int K) 
+	{ 
+		// Calculate the total no. of subsets 
+		int x = (int)Math.pow(2, arr.length); 
+
+		// Run loop till total no. of subsets 
+		// and call the function for each subset 
+		for (int i = 1; i < x; i++) 
+			sumSubsets(arr, i, K); 
+	} 
+
+	// Driver code 
+	public static void main(String args[]) 
+	{ 
+		int arr[] = { 5, 10, 12, 13, 15, 18 }; 
+		int K = 30; 
+
+		findSubsets(arr, K); 
+	} 
+} 
