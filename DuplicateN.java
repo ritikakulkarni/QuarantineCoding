431d43d7b7f803a5f39d27430f4a2fefa189b0d3
ritikakulkarni <65144317+ritikakulkarni@users.noreply.github.com>
2020-06-09 15:48:18 +0530

    Add files via upload

---
+++ DuplicateN.java
@@ -0,0 +1,29 @@
+public class DuplicateN
+ { 
+	public static void cntArray(int A[], int N) 
+	{ 
+		int result = 0; 
+		for (int i = 0; i < N; i++)
+ { 
+			result++; 
+			int current_value = A[i]; 
+			for (int j = i + 1; j < N; j++)
+ { 
+
+				// Check if A[j] = A[i] 
+				// increase result by 1 
+				if (A[j] == current_value) { 
+					result++; 
+				} 
+			} 
+		} 
+		System.out.println(result); 
+	} 
+	public static void main(String[] args) 
+	{ 
+		int[] A = { 1, 5, 6, 1, 9, 
+					5, 8, 10, 8, 9 }; 
+		int N = A.length; 
+		cntArray(A, N); 
+	} 
+} 
