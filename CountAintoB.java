08bc325b338d8b5c875d4b640fa140261f62b761
ritikakulkarni <65144317+ritikakulkarni@users.noreply.github.com>
2020-06-20 11:04:51 +0530

    Add files via upload

---
+++ CountAintoB.java
@@ -0,0 +1,29 @@
+ import java.util.*; 
+public class CountAintoB { 
+
+	public static int countSetBits(int n) 
+	{ 
+		int count = 0; 
+		while (n != 0) { 
+			count++; 
+			n &=(n-1); 
+		} 
+		return count; 
+	} 
+
+	// Function that return count of 
+	// flipped number 
+	public static int FlippedCount(int a, int b) 
+	{ 
+		// Return count of set bits in 
+		// a XOR b 
+		return countSetBits(a ^ b); 
+	} 
+	
+	public static void main(String[] args) 
+	{ 
+		int a = 10; 
+		int b = 20; 
+		System.out.print(FlippedCount(a, b)); 
+	} 
+} 
